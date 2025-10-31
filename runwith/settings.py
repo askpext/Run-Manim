10385ab40c7e33922059a95b2ac95b1e289d025d
thesophile <abhinavpb249@gmail.com>
2025-10-31 21:11:28 +0530

    media url update

--- runwith/settings.py
+++ runwith/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 #I entered this for media
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://runwith.cloud/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
